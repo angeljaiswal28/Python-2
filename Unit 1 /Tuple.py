a1e0ebca5bb33f4c0ea62605a8d294fb2a81f09d
angeljaiswal28 <angeljaiswal2820@gmail.com>
2026-03-07 00:19:12 +0530

    Create Tuple.py

---
+++ Unit 1 /Tuple.py
@@ -0,0 +1,14 @@
+#Nested tuple
+nested_tuple = (1, (2, 3), 4)
+print("Nested tuple:", nested_tuple)
+print("Access inner tuple:", nested_tuple[1])  # Output: (2, 3)
+
+#Repetition of tuple
+tuple1 = (1, 2)
+repeated_tuple = tuple1 * 3
+print("Repeated tuple:", repeated_tuple)  # Output: (1, 2, 1, 2, 1, 2)
+
+#Concatenation of tuples
+tuple2 = (3, 4)
+concat_tuple = tuple1 + tuple2
+print("Concatenated tuple:", concat_tuple)  # Output: (1, 2, 3, 4)
