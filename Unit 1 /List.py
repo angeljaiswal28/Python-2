ab51917c63d85dfe9dd22c02271d10027d92df0b
angeljaiswal28 <angeljaiswal2820@gmail.com>
2026-03-07 00:17:50 +0530

    Create List.py

---
+++ Unit 1 /List.py
@@ -0,0 +1,12 @@
+l1=['d','b','a','c']
+print("Orignal list = ",l1)
+#Accessing elemnt
+print("Element at position 2 = ",l1[1])
+#Adding element
+l1.append('e');print("List after adding element : ",l1)
+#Removing element
+l1.pop(4);print("List after removing element : ",l1)
+#sorting
+l1.sort;print("Sorted list : ",l1)
+#Reverse
+l1.reverse;print("Reversed list : ",l1) 
