c15c06b3f444b994470ad9893213ad1d28050d76
angeljaiswal28 <angeljaiswal2820@gmail.com>
2026-03-24 12:11:18 +0530

    Update shapes.py

--- Unit 3 /shapes.py
+++ Unit 3 /shapes.py
@@ -1,15 +1,7 @@
-
 import math
-
-def get_oval_surface(rad):
-    """Calculates the surface area of a circle."""
-    return math.pi * rad * rad
-
-def get_quad_surface(side_a, side_b):
-    """Calculates the surface area of a four-sided polygon."""
-    return side_a * side_b
-
-def get_slope_surface(base_val, height_val):
-    """Calculates the surface area of a triangular shape."""
-    return 0.5 * base_val * height_val
-  
+def circle_area(r):
+    return math.pi*r*r
+def rectangle_area(l, b):
+    return l*b
+def triangle_area(l, b):
+    return 0.5*l*b
